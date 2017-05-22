1e2190016c5cddda1618e5044cdb04de5cdb575d
fritzmg <fmg@inspiredminds.at>
2017-05-22 15:30:42 +0200

    english translations

---
+++ system/modules/css-style-selector/languages/en/modules.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Extension for Contao Open Source CMS
+ *
+ * Copyright (c) 2015 Craffft
+ *
+ * @package CssStyleSelector
+ * @link    https://github.com/craffft/contao-css-style-selector
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['cssStyleSelector'] = array('CSS styles', 'You can define CSS styles with predefined CSS classes here.');
